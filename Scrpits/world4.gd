87297d6c7cfcab5fa125f3efe332b528d81aa373
ahmed0097 <ahmed.zghal1233@gmail.com>
2022-11-01 15:36:03 +0100

    level BG Music

--- Scrpits/world4.gd
+++ Scrpits/world4.gd
@@ -4,11 +4,12 @@ extends Node2D
 onready var MG4=get_node("Node/AudioStreamPlayer")
 
 # Called when the node enters the scene tree for the first time.
+#background music level
 func _ready():
 	MG4.play()
 	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+# Called every frame. 'delta' is the elapsed time since the previous frame
 #func _process(delta):
 #	pass
