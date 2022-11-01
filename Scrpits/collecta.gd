2ac03a62b43660070edd6460f4a299711aea7364
ahmed0097 <ahmed.zghal1233@gmail.com>
2022-11-01 15:50:16 +0100

    Collectables script

--- Scrpits/collecta.gd
+++ Scrpits/collecta.gd
@@ -1,11 +1,6 @@
 extends Area2D
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
+#collectables
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$AnimatedSprite.play("idel")
